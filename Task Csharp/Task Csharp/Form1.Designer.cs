ea6abecb94b95ad232a6e88eb1d122d046b6f13f
Nguyễn Đức Hưng <48803420+NguyenDucHung1601@users.noreply.github.com>
2020-08-17 09:00:40 +0700

    Task Csharp

---
+++ Task Csharp/Task Csharp/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Task_Csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonAPI = new System.Windows.Forms.Button();
+            this.textBoxAPI = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bấm nút sau để lấy API";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(123, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "API lấy được";
+            // 
+            // buttonAPI
+            // 
+            this.buttonAPI.BackColor = System.Drawing.Color.YellowGreen;
+            this.buttonAPI.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAPI.Location = new System.Drawing.Point(263, 81);
+            this.buttonAPI.Name = "buttonAPI";
+            this.buttonAPI.Size = new System.Drawing.Size(159, 41);
+            this.buttonAPI.TabIndex = 2;
+            this.buttonAPI.Text = "Bấm vào đây";
+            this.buttonAPI.UseVisualStyleBackColor = false;
+            this.buttonAPI.Click += new System.EventHandler(this.buttonAPI_Click);
+            // 
+            // textBoxAPI
+            // 
+            this.textBoxAPI.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAPI.Location = new System.Drawing.Point(263, 224);
+            this.textBoxAPI.Multiline = true;
+            this.textBoxAPI.Name = "textBoxAPI";
+            this.textBoxAPI.Size = new System.Drawing.Size(512, 196);
+            this.textBoxAPI.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxAPI);
+            this.Controls.Add(this.buttonAPI);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonAPI;
+        private System.Windows.Forms.TextBox textBoxAPI;
+    }
+}
+
---
+++ Task Csharp/Task Csharp/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_Csharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void buttonAPI_Click(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://graph.facebook.com/me?acess_token=abc");
+            HttpResponseMessage respone = await client.GetAsync("api");
+            string result = await respone.Content.ReadAsStringAsync();
+            textBoxAPI.Text = result;
+        }
+    }
+}
